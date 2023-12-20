f5e50d3d7d44da24d0cd0caa5f549510ab1601c8
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-12-20 18:19:32 +0500

    feat - integrated signout api and about and home component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -100,6 +100,15 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/auth/signout");
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -165,7 +174,9 @@ export default function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
       <p className="text-green-700 mt-5">
